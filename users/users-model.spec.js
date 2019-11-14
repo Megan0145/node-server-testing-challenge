5dad6f9ea990692e8a16847910f3b195634aba27
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-14 14:59:30 +0000

    Test to ensure user names unique

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -2,15 +2,25 @@ const Users = require("./users-model");
 const db = require("../data/db-config");
 
 beforeEach(() => {
-    return db("users").truncate();
-})
+  return db("users").truncate();
+});
 
 describe("Users model", () => {
-    describe("Insert fn", () => {
-        test("Should insert user to db", async () => {
-            await Users.insert({ name: "New User 1" })
-            const users = await db("users");
-            expect(users).toHaveLength(1)
-        })
-    })
-})
+  describe("Insert fn", () => {
+    test("Should insert user to db", async () => {
+      await Users.insert({ name: "New User 1" });
+      const users = await db("users");
+      expect(users).toHaveLength(1);
+    });
+    test("Should only allow unique names", () => {
+      async () => {
+        try {
+          await Users.insert({ name: "Megan" });
+          await Users.insert({ name: "Megan" });
+        } catch (error) {
+          expect(error.code).toBe("SQLITE_CONSTRAINT");
+        }
+      };
+    });
+  });
+});
