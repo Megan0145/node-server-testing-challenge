a259432edc3f532f9efb8de6f6f72c27adc33169
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-14 14:15:31 +0000

    Add test to ensure db env is testing

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,3 +1,10 @@
-it ("bla bla", () => {
-    
+const request = require("supertest");
+const server = require("./server");
+
+describe("server", () => {
+    describe("[GET] / endpoint", () => {
+       test("the db env is testing", () => {
+           expect(process.env.DB_ENV).toBe("testing");
+       })
+    })
 })
