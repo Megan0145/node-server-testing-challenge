7c4b5381c1481e7a213de6c2794b192b77a6e50b
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-14 13:56:25 +0000

    Endpoint to delete user

--- api/server.js
+++ api/server.js
@@ -16,11 +16,9 @@ server.get("/users", (req, res) => {
       res.status(200).json(users);
     })
     .catch(err => {
-      res
-        .status(500)
-        .json({
-          message: "Something went wrong trying to get users: " + err.message
-        });
+      res.status(500).json({
+        message: "Something went wrong trying to get users: " + err.message
+      });
     });
 });
 
@@ -34,6 +32,19 @@ server.post("/users", (req, res) => {
       res
         .status(500)
         .json({ message: "Something went wrong adding user: " + err.message });
+    });
+});
+
+server.delete("/users/:id", (req, res) => {
+  users
+    .remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: "User deleted", id: req.params.id });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "Could not delete user: " + err.message });
     });
 });
 
