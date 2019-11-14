20ec44e9e05a009543646a76900fcedbcc5892b0
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-14 13:39:24 +0000

    Endpoint to get all users from db

--- api/server.js
+++ api/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const server = express();
 
+const users = require('../users/users-model');
+
 server.use(express.json());
 
 server.get("/", (req, res) => {
   res.status(200).send("Up and running");
 });
+
+server.get("/users", (req, res) => {
+    users.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ message: "Something went wrong trying to get users: " + err.message })
+    })
+})
 
 module.exports = server;
