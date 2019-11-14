e9df837623430c1a70e9c59e63d651d4e93c3652
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-14 14:27:32 +0000

    test insert helper method

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -1,3 +1,16 @@
-it ("bla bla", () => {
-    
+const Users = require("./users-model");
+const db = require("../data/db-config");
+
+beforeEach(() => {
+    return db("users").truncate();
+})
+
+describe("Users model", () => {
+    describe("Insert fn", () => {
+        test("Should insert user to db", async () => {
+            await Users.insert({ name: "New User 1" })
+            const users = await db("users");
+            expect(users).toHaveLength(1)
+        })
+    })
 })
